9f313697ba367c2b99ac4ed5ec454e6e51c1e164
famahana <famahana55@gmail.com>
2026-08-26 11:50:23 +0200

    AddedFooterAndUpperBackgroundWithSearch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import MainPage from './Components/MainPage/MainPage'
+import Hero from './Components/Hero/Hero'
 import "./Main.css"
 
 function App() {
   return (
-    <>
-    <Header/>
-      
-    </>
+    <div className="app_wrapper">
+      <Header/>
+      <main className='main_content'>
+        <Hero/>
+        <MainPage/>
+      </main>
+      <Footer/>
+    </div>
   )
 }
 
---
+++ src/Components/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import "./MainPage.css"
+const MainPage = ()=>{
+    return(
+        <div className="main_page">
+            1
+        </div>
+    )
+}
+export default MainPage
