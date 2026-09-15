0a55571655ed1510c1d9eff78dc70568f5e849ee
Misha846-ux <michael@gladkov.com.ua>
2026-09-15 11:15:36 +0200

    Fix second page

--- src/Components/MainPage/MainPage.tsx
+++ src/Components/MainPage/MainPage.tsx
@@ -1,12 +1,10 @@
 import "./MainPage.css"
-import MiniHeader from "../ExtraMainText/MiniHeader/MiniHeader";
 import Places from "../Places/Places";
 import Reviews from "../Reviews/Reviews";
 import SafeWithUs from "../ExtraMainText/SafeWithUs/SafeWithUs";
 import BeOurRegular from "../ExtraMainText/BeOurRegular/BeOurRegular";
 import RegisterAccountButton from "../RegisterAccountButton/RegisterAccountButton";
 import data from "../Temporary_json_files/data.json";
-import Hero from "../Hero/Hero";
 
 const MainPage = ()=>{
     const reviews = data.reviews;
@@ -18,8 +16,6 @@ const MainPage = ()=>{
     
     return(
         <div className="main_page">
-            <Hero></Hero>
-            <MiniHeader></MiniHeader>
             <Places hotels={hotels} cities={cities} countries={countries} rooms={rooms}></Places>
             <Reviews reviews={reviews} users={users} hotels={hotels}></Reviews>
             <SafeWithUs></SafeWithUs>
--- src/RouterComponents/HotelsRouts/HotelsGeneralBackGround.tsx
+++ src/RouterComponents/HotelsRouts/HotelsGeneralBackGround.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../../Components/Footer/Footer";
+import Hero from "../../Components/Hero/Hero";
+import MiniHeader from "../../Components/ExtraMainText/MiniHeader/MiniHeader";
 
 const HotelsGeneralBackGround = () => {
 	return (
 		<>
+			<Hero />
+			<MiniHeader />
 			<Outlet />
 			<Footer />
 		</>
